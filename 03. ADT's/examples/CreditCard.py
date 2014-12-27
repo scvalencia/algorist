bf96151876674b35e84f5eed96360013da4ccb1b
Sebastián Valencia <sc.valencia606@gmail.com>
2014-12-26 23:43:05 -0500

    Some changes

--- 03. ADT's/examples/CreditCard.py
+++ 03. ADT's/examples/CreditCard.py
@@ -15,26 +15,42 @@ class CreditCard(object):
 		self.bank = bank
 		self.acnt = acnt
 		self.limit = limit
-		self.balance
+		self.balance = 0
 
-		def charge(self, price):
-			''' Charge given price to the card, assumming sufficient credit limit
+	def charge(self, price):
+		''' Charge given price to the card, assumming sufficient credit limit
 
-			args:
-				price (float) : price to be charged
+		args:
+			price (float) : price to be charged
 
-			limit:
-				boolean : a flag indicating if the charge was processed
-			'''
+		returns:
+			boolean : a flag indicating if the charge was processed
+		'''
 
-			new_balance = price + self.balance
+		new_balance = price + self.balance
 
-			if new_balance > self.limit:
-				return False
-			else:
-				self.balance = new_balance
-				return True
+		if new_balance > self.limit:
+			return False
+		else:
+			self.balance = new_balance
+			return True
 
-		def make_payment(self, price):
-			'''
+	def make_payment(self, amount):
+		''' Processes customer payment that reduces balance 
+
+		args:
+			amount (float) : the amount of dollars to be removed from
+						     the balance
+
+		returns:
+			boolean : a flag showing if the transaction was successful
+		'''
+
+		new_balance = self.balance - amount
+
+		if new_balance < 0:
+			return False
+		else:
+			self.balance = new_balance
+			return True
 		
