b05bc3f4dbbf74845e6a5aa8e3211c7479f77672
Sebastián Valencia <sc.valencia606@gmail.com>
2014-12-26 23:34:46 -0500

    Some silly Object oriented programming example

---
+++ 03. ADT's/examples/CreditCard.py
@@ -0,0 +1,40 @@
+class CreditCard(object):
+
+	def __init__(self, customer, bank, acnt, limit):
+		''' Creates a new credit card instance.
+
+		The initial balance is zero
+		args:
+			customer (string): name of the customer (e.g. 'John Lynch')
+			bank (string)    : name of thr bank (e.g 'Bank of San Serriffe')
+			acnt (strign)    : account identifier (e.g '12321 343534 454545 43434')
+			limit (float)    : credit limit measured in dollars
+		'''
+
+		self.customer = customer
+		self.bank = bank
+		self.acnt = acnt
+		self.limit = limit
+		self.balance
+
+		def charge(self, price):
+			''' Charge given price to the card, assumming sufficient credit limit
+
+			args:
+				price (float) : price to be charged
+
+			limit:
+				boolean : a flag indicating if the charge was processed
+			'''
+
+			new_balance = price + self.balance
+
+			if new_balance > self.limit:
+				return False
+			else:
+				self.balance = new_balance
+				return True
+
+		def make_payment(self, price):
+			'''
+		
