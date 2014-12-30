de96934cb1201cf81df6174497ce737d35e287ce
Sebastián Valencia <sc.valencia606@gmail.com>
2014-12-29 21:13:46 -0500

    AND logic gate

--- 03. ADT's/examples/LogicGate.py
+++ 03. ADT's/examples/LogicGate.py
@@ -10,6 +10,58 @@ class LogicGate(object):
 		return self.label
 
 	def get_output(self):
-		
+
 		self.output = self.performGateLogic()
 		return self.output
+
+class BinaryGate(LogicGate):
+
+	def __init__(self, name):
+
+		LogicGate.__init__(self, name)
+
+		self.pin_a = None
+		self.pin_b = None
+
+	def get_pinA(self):
+
+		ipt = input("Enter Pin A input for gate " + self.get_label() + " --> ")
+		self.pin_a = int(ipt)
+		return self.pin_a
+
+	def get_pinB(self):
+		
+		ipt = input("Enter Pin B input for gate " + self.get_label() + " --> ")
+		self.pin_b = int(ipt)
+		return self.pin_b
+
+class UnaryGate(LogicGate):
+
+	def __init__(self, name):
+
+		LogicGate.__init__(self, name)
+
+		self.pin = None
+
+	def get_pin(self):
+
+		ipt = input("Enter Pin input for gate " + self.get_label() + " --> ")
+		self.pin = int(ipt)
+		return self.pin
+
+class AndGate(BinaryGate):
+
+	def __init__(self, name):
+
+		BinaryGate.__init__(self, name)
+
+	def performGateLogic(self):
+
+		a = self.get_pinA()
+		b = self.get_pinB()
+
+		if a == 1 and b == 1:
+			return a
+
+		else:
+			return 0
