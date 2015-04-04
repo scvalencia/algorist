23dcddbdbaf8bc70c7e075e51a13f12c73791c4e
cps543 <sc.valencia606@gmail.com>
2015-04-04 17:30:47 -0500

    Creative exercises from Goodrich, et al.

--- 02. Algorithm analysis/exercises/Goodrich.py
+++ 02. Algorithm analysis/exercises/Goodrich.py
@@ -158,4 +158,60 @@ def lacking_number(lst, n):
 	return summation - accum
 
 # C-3.50
+def naive_computation(p, x):
+	ans = 0.0
+
+	for i, itm in enumerate(p):
+		
+		accum = 1.0
+		for j in range(i):
+			accum *= x
+
+		ans += itm * accum
+
+	return ans
+
+def power(x, n):
+	if n == 0: 
+		return 1
+	if n % 2 == 0:
+		m = power(x, n / 2)
+		return m * m
+	else:
+		return x * power(x, n - 1)
+
+def optimized_computation(p, x):
+
+	ans = 0.0
+
+	for i, itm in enumerate(p):
+		ans += itm * power(x, i)
+
+	return ans
+
+# O(n)
+def horner(p, x):
+	'''
+		Given p(x) = a + bx + c^2 + dx^3
+		p(x) = a + x(b + x(c + dx))
+
+		t1 = d
+		t1 = t1 * x
+		t1 = t1 + c
+		t1 = t1 * x
+		t1 = t1 + b
+		t1 = t1 * x
+		t1 = t1 + a
+
+		t1 = p(x)
+	'''
+	
+	i = len(p) - 1
+	t1 = p[i]
+
+	while i > 0:
+		t1 = t1 * x + p[i - 1]
+		i -= 1
+
+	return t1
 
