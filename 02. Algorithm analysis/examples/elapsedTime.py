28e07bb0d8f7643b540459ccb6a96e680d5a9a9a
cps543 <sc.valencia606@gmail.com>
2015-04-04 11:57:16 -0500

    Results of experimental algorithm analysis on Linear-sequential search and Binary search

--- 02. Algorithm analysis/examples/elapsedTime.py
+++ 02. Algorithm analysis/examples/elapsedTime.py
@@ -1,6 +1,7 @@
 from time import time
 from random import random
 from random import choice
+import matplotlib.pyplot as plt
 
 
 def linear_search(collection, target):
@@ -30,7 +31,7 @@ def binary_search(collection, target):
 def main():
 
 	sizes = [i for i in range(0, 11000, 1000)]
-	lists = [[int(1000 * random()) for i in xrange(size)] for size in sizes if size != 0]
+	lists = [sorted([int(1000 * random()) for i in xrange(size)]) for size in sizes if size != 0]
 	results = {}
 	
 	target = 57
@@ -46,13 +47,18 @@ def main():
 
 		results[len(lst)] = (linear_time, binary_time)
 
+	linear = {i : results[i][0] for i in results}
+	binary = {i : results[i][1] for i in results}
+
 	print 'n \t Linear Search \t Binary Search'
 	print '-' * 37
 
 	for itm in sizes[1:]:
 		print "%d \t %.8f \t %.8f " % (itm, results[itm][0], results[itm][1])
 
-
+	plt.bar(linear.keys(), linear.values(), 100, color='g')
+	plt.bar(binary.keys(), binary.values(), 100, color='r')
+	plt.show()
 
 if __name__ == '__main__':
 	main()
