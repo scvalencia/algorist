0449a6d3c2faf8d2f386a7744622280c9c13d0e8
scvalencia <sc.valencia606@uniandes.edu.co>
2015-09-22 23:57:35 -0500

    Reed and Zelle's book on Data structures and algorithms in Python and C++, Ch 2, ADT's

--- 03. Arrays and ADT's/exercises/Reed.py
+++ 03. Arrays and ADT's/exercises/Reed.py
@@ -277,6 +277,45 @@ class Dataset(object):
 
 # P-2.10
 
+def gcd(m, n):
+
+	while m % n != 0:
+		oldm = m
+		oldn = n
+
+		m = oldn
+		n = oldm % oldn
+
+	return n
+
+class Rational(object):	
+
+	def __init__(self, top, bottom):
+
+		self.num = top
+		self.den = bottom
+
+	def __add__(self, other):
+
+		new_num = self.num * other.den + self.den * other.num
+		new_den = self.den * other.den
+		common = gcd(new_num, new_den)
+
+		return Fraction(new_num // common, new_den // common)
+
+	def __eq__(self, other):
+
+		firstnum = self.num * other.den
+		secondnum = other.num * self.den
+
+		return firstnum == secondnum
+
+	def __str__(self):
+		return str(self.num) + "/" + str(self.den)
+
+	def __repr__(self):
+		return str(self)
+
 # P-2.11
 
 # P-2.12
