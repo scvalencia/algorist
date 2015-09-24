79373c5b75753f38e5a382035f3dff934790b42f
scvalencia <sc.valencia606@uniandes.edu.co>
2015-09-24 11:52:22 -0500

    An ADT for representing a deck of cards

--- 03. Arrays and ADT's/examples/Deck.py
+++ 03. Arrays and ADT's/examples/Deck.py
@@ -1,22 +1,73 @@
+import random
+
+class Card(object):
+
+	SUITS = 'cdhs'
+	SUIT_NAMES = ['clubs', 'diamonds', 'hearts', 'spades']
+	RANK = range(1, 14)
+	RANK_NAMES = ['Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 
+				'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King']
+
+	def __init__(self, suit, rank):
+		self.rank_num = rank
+		self.suit_char = suit
+
+	def suit(self):
+		return self.suit_char
+
+	def rank(self):
+		return self.rank_num
+
+	def suit_name(self):
+		index = self.SUITS.index(self.suit_char)
+		return self.SUIT_NAMES[index]
+
+	def rank_name(self):
+		index = self.RANK.index(self.rank_num)
+		return self.RANK_NAMES[index]
+
+	def __str__(self):
+		return self.rank_name() + ' of ' + self.suit_name()
+
 class Deck(object):
 
 	def __init__(self):
 		'''Creates a new Deck object
 		post: create a 52-card deck in standard order
 		'''
+		cards = []
+
+		for suit in Card.SUITS:
+			for rank in Card.RANK:
+				card = Card(suit, rank)
+				cards.append(card)
+
+		self.cards = cards
 
 	def shuffle(self):
 		'''Shuffle the deck of cards
 		post: randomizes the order of the cards in the deck
 		'''
+		current_deck = self.cards
+		cards = []
+
+		while current_deck != []:
+			position = random.randint(0, self.size() - 1)
+			card = current_deck.pop(position)
+
+			cards.append(card)
+
+		self.cards = cards
 
 	def size(self):
 		'''Crads left
 		post: returns the number of cards in self
 		'''
+		return len(self.cards)
 
 	def deal(self):
 		'''Deal a single card
 		pre: self <> NULL self.size() > 0
 		post: returns the next card in the deck, and removes it from the deck
 		'''
+		return self.cards.pop()
